9be85d8538c6be59aac73f78f037a5c822e947a0
99p <poccori@gmail.com>
2021-11-06 14:32:23 +0900

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,30 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @nice = "home"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "#{@nice}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "help"
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "about"
+  end
+
+  teardown do
+  end
+
 end
